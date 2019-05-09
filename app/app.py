3f69421c0d4626b89c64434df972f85a1d834b31
Nima Shoghi <nimashoghi@gmail.com>
2019-05-09 15:32:13 -0400

    Added OAuth server

--- app/app.py
+++ app/app.py
@@ -1,16 +1,24 @@
+import mimetypes
+import os.path
 from typing import Callable
 
+import requests
 from requests_oauthlib import OAuth2Session
 
 from server import get_oauth_code, host, port
 
-client_id = "f0c1685729f99b7e953f2"
-client_secret = "68eadbf5496e28d60f010"
+client_id = "6640ad8c00ead54453601"
+client_secret = "01e8f0bcfadd28fb35aeb"
 redirect_uri = f"http://{host}:{port}"
 scope = ["podcast_read", "podcast_update", "episode_read", "episode_publish"]
 
 oauth_url = "https://api.podbean.com/v1/dialog/oauth"
 token_url = "https://api.podbean.com/v1/oauth/token"
+
+get_podcasts_url = "https://api.podbean.com/v1/podcasts"
+get_episodes_url = "https://api.podbean.com/v1/episodes"
+authorize_upload_url = "https://api.podbean.com/v1/files/uploadAuthorize"
+publish_episode_url = "https://api.podbean.com/v1/episodes"
 
 
 def authorize(oauth: OAuth2Session):
@@ -19,13 +27,117 @@ def authorize(oauth: OAuth2Session):
     return get_oauth_code()
 
 
-def get_token(code: str, oauth: OAuth2Session):
+def get_access_token(code: str, oauth: OAuth2Session):
     return oauth.fetch_token(
         token_url=token_url, code=code, client_id=client_id, client_secret=client_secret
+    )["access_token"]
+
+
+def build_params(access_token, **kwargs):
+    return dict(access_token=access_token, **kwargs)
+
+
+def get_podcasts(access_token: str):
+    result = requests.get(get_podcasts_url, params=build_params(access_token)).json()
+    return result["podcasts"]
+
+
+def get_episodes(access_token: str, podcast_id: str):
+    result = requests.get(
+        get_episodes_url, params=build_params(access_token, podcast_id=podcast_id)
+    ).json()
+    return result["episodes"]
+
+
+def get_file_name(path: str):
+    return os.path.basename(path)
+
+
+def get_file_size(path: str):
+    return os.path.getsize(path)
+
+
+def get_content_type(path: str):
+    type, _ = mimetypes.guess_type(path)
+    return type if type else "audio/mpeg"
+
+
+def authorize_upload(access_token: str, file_path: str):
+    result = requests.get(
+        authorize_upload_url,
+        params=build_params(
+            access_token,
+            filename=get_file_name(file_path),
+            filesize=get_file_size(file_path),
+            content_type=get_content_type(file_path),
+        ),
+    ).json()
+    return result["presigned_url"], result["file_key"]
+
+
+def upload_file(file_path: str, presigned_url: str):
+    return requests.put(
+        presigned_url,
+        headers={"Content-Type": get_content_type(file_path)},
+        data=open(file_path, "rb"),
     )
 
 
-oauth = OAuth2Session(client_id=client_id, redirect_uri=redirect_uri, scope=scope)
-code = authorize(oauth)
-token_result = get_token(code, oauth)
-access_token = token_result["access_token"]
+def publish_episode(
+    access_token: str,
+    file_key: str,
+    title: str,
+    content="",
+    status="publish",
+    type="public",
+):
+    result = requests.post(
+        publish_episode_url,
+        data=build_params(
+            access_token,
+            title=title,
+            content=content,
+            status=status,
+            type=type,
+            media_key=file_key,
+        ),
+    ).json()
+
+    return result["episode"]
+
+
+def upload_new_media(access_token: str, file_path: str):
+    presigned_url, file_key = authorize_upload(access_token, file_path)
+    upload_file(file_path, presigned_url)
+    return publish_episode(access_token, file_key, get_file_name(file_path))
+
+
+def get_new_access_token():
+    oauth = OAuth2Session(client_id=client_id, redirect_uri=redirect_uri, scope=scope)
+    oauth._client.grant_type = "client_credentials"
+    code = authorize(oauth)
+    return get_access_token(code, oauth)
+
+def main():
+    access_token = "3755babd3002cfcbac020451d6456b85a1ad7424"
+    print(f"Your access token is {access_token}")
+
+    # print(
+    #     authorize_upload(access_token, f"{os.path.dirname(__file__)}/bensound-summer.mp3")
+    # )
+    # print(
+    #     upload_file(f"{os.path.dirname(__file__)}/bensound-summer.mp3", presigned_url).text
+    # )
+    # get_content_type(f"./app/bensound-summer.mp3")
+    access_token = get_new_access_token()
+    print(
+        upload_new_media(
+            access_token, file_path=f"{os.path.dirname(__file__)}/bensound-creativeminds.mp3"
+        )
+    )
+
+if __name__ == "__main__":
+    from multiprocessing import freeze_support
+    freeze_support()
+
+    main()
--- app/server.py
+++ app/server.py
@@ -1,5 +1,5 @@
 from ctypes import c_char_p
-from multiprocessing import Manager, Process
+from multiprocessing import Manager, Process, set_start_method
 from time import sleep
 
 from flask import Flask, request
@@ -7,27 +7,35 @@ from flask import Flask, request
 host = "127.0.0.1"
 port = "23808"
 
+set_start_method("spawn", True)
+
+
 def disable_logging():
     import logging
-    log = logging.getLogger('werkzeug')
+
+    log = logging.getLogger("werkzeug")
     log.disabled = True
 
+
+def run_app(code):
+
+    app = Flask("OAuthServer")
+    disable_logging()
+
+    @app.route("/")
+    def callback():  # pylint: disable=unused-variable
+        request_code = request.args.get("code")
+
+        if not request_code:
+            return "Failed to authorize!"
+
+        code.value = request_code
+        return "Successfully authorized!"
+
+    app.run(host=host, port=port)
+
+
 def get_oauth_code():
-    def run_app(code):
-        app = Flask("OAuthServer")
-        disable_logging()
-
-        @app.route("/")
-        def callback():  # pylint: disable=unused-variable
-            request_code = request.args.get("code")
-
-            if not request_code:
-                return "Failed to authorize!"
-
-            code.value = request_code
-            return "Successfully authorized!"
-
-        app.run(host=host, port=port)
 
     manager = Manager()
     code = manager.Value(c_char_p, "")
