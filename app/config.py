54ff421f837196339846e7440a0d6ccde93d6bee
Nima Shoghi <nimashoghi@gmail.com>
2019-05-10 22:36:20 -0400

    Mounted pickles for long-term storage

--- app/config.py
+++ app/config.py
@@ -9,8 +9,8 @@ channel_id = environ["YOUTUBE_CHANNEL_ID"]
 title_pattern = environ.get("YOUTUTBE_TITLE_PATTERN", default=".+")
 
 access_code_pickle_path = environ.get(
-    "ACCESS_CODE_PICKLE_PATH", default="access_code.pickle"
+    "ACCESS_CODE_PICKLE_PATH", default="pickles/access_code.pickle"
 )
 published_after_pickle_path = environ.get(
-    "PUBLISHED_AFTER_PICKLE_PATH", default="published_after.pickle"
+    "PUBLISHED_AFTER_PICKLE_PATH", default="pickles/published_after.pickle"
 )
