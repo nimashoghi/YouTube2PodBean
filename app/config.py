3c879514edc44fa19a4451ec83dc229f2864176a
Nima Shoghi <nimashoghi@gmail.com>
2019-05-10 22:23:40 -0400

    Added a way to get settings from environment variables

--- app/config.py
+++ app/config.py
@@ -1,10 +1,16 @@
-client_id = ""
-client_secret = ""
+from os import environ
 
-youtube_api_key = ""
-channel_id = ""
+client_id = environ["PODBEAN_CLIENT_ID"]
+client_secret = environ["PODBEAN_CLIENT_SECRET"]
 
-title_pattern = "doodle"
+youtube_api_key = environ["YOUTUBE_API_KEY"]
+channel_id = environ["YOUTUBE_CHANNEL_ID"]
 
-access_code_pickle_path = "access_code.pickle"
-published_after_pickle_path = "published_after.pickle"
+title_pattern = environ.get("YOUTUTBE_TITLE_PATTERN", default=".+")
+
+access_code_pickle_path = environ.get(
+    "ACCESS_CODE_PICKLE_PATH", default="access_code.pickle"
+)
+published_after_pickle_path = environ.get(
+    "PUBLISHED_AFTER_PICKLE_PATH", default="published_after.pickle"
+)
