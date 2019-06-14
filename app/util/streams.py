a4ec7f24ea66fc1e726b4439591de57075541a17
Nima Shoghi <nimashoghi@gmail.com>
2019-06-14 22:56:19 +0000

    Added aiomqtt support

--- app/util/streams.py
+++ app/util/streams.py
@@ -2,43 +2,97 @@ import asyncio
 import pickle
 from contextlib import asynccontextmanager
 from logging import getLogger
-from typing import AsyncIterator, List
+from typing import AsyncIterator, Callable, List
 
-from hbmqtt.client import QOS_2, MQTTClient
-from hbmqtt.mqtt.publish import PublishPacket, PublishPayload
-from hbmqtt.session import ApplicationMessage
+import aiomqtt
 from pafy.backend_youtube_dl import YtdlPafy
 
 logging = getLogger(__name__)
 
 
 @asynccontextmanager
-async def create_client():
+async def create_client(manage_loop=True):
     from app.config.core import message_broker
 
     message_broker = await message_broker()
+    loop = asyncio.get_event_loop()
+    client = aiomqtt.Client()
+
     try:
-        client = MQTTClient()
+        logging.debug(f"Starting MQTT loop")
+        client.loop_start()
+        logging.debug(f"Started MQTT loop")
+
         logging.debug(f"Connecting to message broker at '{message_broker}'")
+
+        # connect
+        connected = asyncio.Event(loop=loop)
+
+        def on_connect(client, userdata, flags, rc):
+            connected.set()
+
+        client.on_connect = on_connect
+
         await client.connect(message_broker)
+        await connected.wait()
+
         logging.debug(f"Connected to message broker at '{message_broker}'")
+
         yield client
+    except BaseException as e:
+        print(f"got exception {e}")
     finally:
+        # disconnect
         logging.debug(f"Disconnecting from message broker at '{message_broker}'")
-        await client.disconnect()
+        disconnected = asyncio.Event(loop=loop)
+
+        def on_disconnect(client, userdata, rc):
+            disconnected.set()
+
+        client.on_disconnect = on_disconnect
+
+        client.disconnect()
+        await disconnected.wait()
         logging.debug(f"Disconnected from message broker at '{message_broker}'")
+
+        if manage_loop:
+            logging.debug(f"Stopping MQTT loop")
+            await client.loop_stop()
+            logging.debug(f"Stopped MQTT loop")
 
 
 @asynccontextmanager
-async def subscribe_to_topic(client: MQTTClient, topic: str):
+async def subscribe_to_topic(client: aiomqtt.Client, topic: str, callback: Callable):
     try:
+        loop = asyncio.get_event_loop()
+
         logging.debug(f"Subscribing to the following MQTT topic: '{topic}'")
-        await client.subscribe([(topic, QOS_2)])
+        subscribed = asyncio.Event(loop=loop)
+
+        def on_subscribe(client, userdata, mid, granted_qos):
+            subscribed.set()
+
+        client.on_subscribe = on_subscribe
+
+        client.subscribe((topic, 2))
+        await subscribed.wait()
+
+        lock = asyncio.Lock(loop=loop)
+
+        def on_message(client, userdata, message):
+            async def handler():
+                async with lock:
+                    await callback(client, userdata, message)
+
+            loop.create_task(handler())
+
+        client.on_message = on_message
+
         logging.debug(f"Subscribed to the following MQTT topic: '{topic}'")
         yield client
     finally:
         logging.debug(f"Unsubscribing to the following MQTT topic: '{topic}'")
-        await client.unsubscribe([topic])  # do not need the QOS for unsub
+        await client.unsubscribe(topic)  # do not need the QOS for unsub
         logging.debug(f"Unsubscribed to the following MQTT topic: '{topic}'")
 
 
@@ -49,17 +103,13 @@ def new_video_event_handler(topic: str, delay=5.0, init=None):
             if init is not None:
                 kwargs = await init()
 
-            async with create_client() as client:
-                async with subscribe_to_topic(client, topic):
-                    while True:
-                        message: ApplicationMessage = await client.deliver_message()
-                        logging.debug(
-                            f"Received a new message from MQTT topic '{message.topic}'"
-                        )
-                        packet: PublishPacket = message.publish_packet
-                        if packet:
-                            payload: PublishPayload = packet.payload
-                            await original_func(pickle.loads(payload.data), **kwargs)
+            async def callback(client, userdata, message):
+                video: YtdlPafy = pickle.loads(message.payload)
+                await original_func(video, **kwargs)
+
+            async with create_client(manage_loop=False) as client:
+                async with subscribe_to_topic(client, topic, callback) as client:
+                    await client.loop_forever()
 
         asyncio.run(fn())
         return original_func
@@ -67,12 +117,14 @@ def new_video_event_handler(topic: str, delay=5.0, init=None):
     return decorator
 
 
-async def send_video(client: MQTTClient, video: YtdlPafy, topics: List[str]):
+async def send_video(client: aiomqtt.Client, video: YtdlPafy, topics: List[str]):
     video_bytes = pickle.dumps(video)
     logging.debug(f"Sending video '{video.title}' to the following topics: '{topics}'")
     await asyncio.gather(
         *(
-            asyncio.ensure_future(client.publish(topic, video_bytes, qos=QOS_2))
+            asyncio.ensure_future(
+                client.publish(topic, video_bytes, qos=2).wait_for_publish()
+            )
             for topic in topics
         )
     )
