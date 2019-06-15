08d9faaac5f81a5d43bbc96b728c306c5afc9037
Nima Shoghi <nimashoghi@gmail.com>
2019-06-15 13:28:50 +0000

    Fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,27 @@
 
 docker login -u $DOCKER_USRENAME -p $DOCKER_PASSWORD
 
-docker build --rm -f "Dockerfile" -t "nimashoghi/youtube2podbean:$TRAVIS_TAG" .
-docker tag "nimashoghi/youtube2podbean:$TRAVIS_TAG" "nimashoghi/youtube2podbean:latest"
-docker push youtube2podbean:$TRAVIS_TAG
-docker push youtube2podbean:latest
+docker build --rm -f "Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean:$TRAVIS_TAG" .
+docker tag "$DOCKER_USRENAME/youtube2podbean:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean:latest"
+docker push $DOCKER_USRENAME/youtube2podbean:$TRAVIS_TAG
+docker push $DOCKER_USRENAME/youtube2podbean:latest
 
-docker build --rm -f "services/discord.Dockerfile" -t "nimashoghi/youtube2podbean-discord:$TRAVIS_TAG" .
-docker tag "nimashoghi/youtube2podbean-discord:$TRAVIS_TAG" "nimashoghi/youtube2podbean-discord:latest"
-docker push youtube2podbean-discord:$TRAVIS_TAG
-docker push youtube2podbean-discord:latest
+docker build --rm -f "services/discord.Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean-discord:$TRAVIS_TAG" ..
+docker tag "$DOCKER_USRENAME/youtube2podbean-discord:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean-discord:latest"
+docker push $DOCKER_USRENAME/youtube2podbean-discord:$TRAVIS_TAG
+docker push $DOCKER_USRENAME/youtube2podbean-discord:latest
 
-docker build --rm -f "services/podbean.Dockerfile" -t "nimashoghi/youtube2podbean-podbean:$TRAVIS_TAG" .
-docker tag "nimashoghi/youtube2podbean-podbean:$TRAVIS_TAG" "nimashoghi/youtube2podbean-podbean:latest"
-docker push youtube2podbean-podbean:$TRAVIS_TAG
-docker push youtube2podbean-podbean:latest
+docker build --rm -f "services/podbean.Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean-podbean:$TRAVIS_TAG" ..
+docker tag "$DOCKER_USRENAME/youtube2podbean-podbean:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean-podbean:latest"
+docker push $DOCKER_USRENAME/youtube2podbean-podbean:$TRAVIS_TAG
+docker push $DOCKER_USRENAME/youtube2podbean-podbean:latest
 
-docker build --rm -f "services/wordpress.Dockerfile" -t "nimashoghi/youtube2podbean-wordpress:$TRAVIS_TAG" .
-docker tag "nimashoghi/youtube2podbean-wordpress:$TRAVIS_TAG" "nimashoghi/youtube2podbean-wordpress:latest"
-docker push youtube2podbean-wordpress:$TRAVIS_TAG
-docker push youtube2podbean-wordpress:latest
+docker build --rm -f "services/wordpress.Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean-wordpress:$TRAVIS_TAG" ..
+docker tag "$DOCKER_USRENAME/youtube2podbean-wordpress:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean-wordpress:latest"
+docker push $DOCKER_USRENAME/youtube2podbean-wordpress:$TRAVIS_TAG
+docker push $DOCKER_USRENAME/youtube2podbean-wordpress:latest
 
-docker build --rm -f "services/youtube.Dockerfile" -t "nimashoghi/youtube2podbean-youtube:$TRAVIS_TAG" .
-docker tag "nimashoghi/youtube2podbean-youtube:$TRAVIS_TAG" "nimashoghi/youtube2podbean-youtube:latest"
-docker push youtube2podbean-youtube:$TRAVIS_TAG
-docker push youtube2podbean-youtube:latest
+docker build --rm -f "services/youtube.Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean-youtube:$TRAVIS_TAG" ..
+docker tag "$DOCKER_USRENAME/youtube2podbean-youtube:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean-youtube:latest"
+docker push $DOCKER_USRENAME/youtube2podbean-youtube:$TRAVIS_TAG
+docker push $DOCKER_USRENAME/youtube2podbean-youtube:latest
