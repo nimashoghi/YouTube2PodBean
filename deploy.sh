717aadedf96e67873ad787dcc6796fe8e1786867
Nima Shoghi <nimashoghi@gmail.com>
2019-06-15 07:03:45 -0400

    Added Dockerfiles for each service Added deployment compose

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,24 @@
 docker build --rm -f "Dockerfile" -t "nimashoghi/youtube2podbean:$TRAVIS_TAG" .
 docker tag "nimashoghi/youtube2podbean:$TRAVIS_TAG" "nimashoghi/youtube2podbean:latest"
+docker push youtube2podbean:$TRAVIS_TAG
+docker push youtube2podbean:latest
+
+docker build --rm -f "services/discord.Dockerfile" -t "nimashoghi/youtube2podbean-discord:$TRAVIS_TAG" .
+docker tag "nimashoghi/youtube2podbean-discord:$TRAVIS_TAG" "nimashoghi/youtube2podbean-discord:latest"
+docker push youtube2podbean-discord:$TRAVIS_TAG
+docker push youtube2podbean-discord:latest
+
+docker build --rm -f "services/podbean.Dockerfile" -t "nimashoghi/youtube2podbean-podbean:$TRAVIS_TAG" .
+docker tag "nimashoghi/youtube2podbean-podbean:$TRAVIS_TAG" "nimashoghi/youtube2podbean-podbean:latest"
+docker push youtube2podbean-podbean:$TRAVIS_TAG
+docker push youtube2podbean-podbean:latest
+
+docker build --rm -f "services/wordpress.Dockerfile" -t "nimashoghi/youtube2podbean-wordpress:$TRAVIS_TAG" .
+docker tag "nimashoghi/youtube2podbean-wordpress:$TRAVIS_TAG" "nimashoghi/youtube2podbean-wordpress:latest"
+docker push youtube2podbean-wordpress:$TRAVIS_TAG
+docker push youtube2podbean-wordpress:latest
+
+docker build --rm -f "services/youtube.Dockerfile" -t "nimashoghi/youtube2podbean-youtube:$TRAVIS_TAG" .
+docker tag "nimashoghi/youtube2podbean-youtube:$TRAVIS_TAG" "nimashoghi/youtube2podbean-youtube:latest"
+docker push youtube2podbean-youtube:$TRAVIS_TAG
+docker push youtube2podbean-youtube:latest
---
+++ services/wordpress.Dockerfile
@@ -0,0 +1,3 @@
+FROM nimashoghi/youtube2podbean:latest
+
+CMD python -m app.services.wordpress
