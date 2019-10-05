b3b903cda1c4abaa1d947112254b71aa2b647579
Nima Shoghi <nimashoghi@gmail.com>
2019-10-05 23:45:57 +0000

    Fixed typo in environment variable name

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-docker login -u $DOCKER_USRENAME -p $DOCKER_PASSWORD
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker build --rm -f "services/Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean:$TRAVIS_TAG" .
-docker tag "$DOCKER_USRENAME/youtube2podbean:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean:latest"
-docker push $DOCKER_USRENAME/youtube2podbean:$TRAVIS_TAG
-docker push $DOCKER_USRENAME/youtube2podbean:latest
+docker build --rm -f "services/Dockerfile" -t "$DOCKER_USERNAME/youtube2podbean:$TRAVIS_TAG" .
+docker tag "$DOCKER_USERNAME/youtube2podbean:$TRAVIS_TAG" "$DOCKER_USERNAME/youtube2podbean:latest"
+docker push $DOCKER_USERNAME/youtube2podbean:$TRAVIS_TAG
+docker push $DOCKER_USERNAME/youtube2podbean:latest
 
-docker build --rm -f "services/discord.Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean-discord:$TRAVIS_TAG" --build-arg BASE_VERSION=$TRAVIS_TAG ..
-docker tag "$DOCKER_USRENAME/youtube2podbean-discord:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean-discord:latest"
-docker push $DOCKER_USRENAME/youtube2podbean-discord:$TRAVIS_TAG
-docker push $DOCKER_USRENAME/youtube2podbean-discord:latest
+docker build --rm -f "services/discord.Dockerfile" -t "$DOCKER_USERNAME/youtube2podbean-discord:$TRAVIS_TAG" --build-arg BASE_VERSION=$TRAVIS_TAG ..
+docker tag "$DOCKER_USERNAME/youtube2podbean-discord:$TRAVIS_TAG" "$DOCKER_USERNAME/youtube2podbean-discord:latest"
+docker push $DOCKER_USERNAME/youtube2podbean-discord:$TRAVIS_TAG
+docker push $DOCKER_USERNAME/youtube2podbean-discord:latest
 
-docker build --rm -f "services/podbean.Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean-podbean:$TRAVIS_TAG" --build-arg BASE_VERSION=$TRAVIS_TAG ..
-docker tag "$DOCKER_USRENAME/youtube2podbean-podbean:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean-podbean:latest"
-docker push $DOCKER_USRENAME/youtube2podbean-podbean:$TRAVIS_TAG
-docker push $DOCKER_USRENAME/youtube2podbean-podbean:latest
+docker build --rm -f "services/podbean.Dockerfile" -t "$DOCKER_USERNAME/youtube2podbean-podbean:$TRAVIS_TAG" --build-arg BASE_VERSION=$TRAVIS_TAG ..
+docker tag "$DOCKER_USERNAME/youtube2podbean-podbean:$TRAVIS_TAG" "$DOCKER_USERNAME/youtube2podbean-podbean:latest"
+docker push $DOCKER_USERNAME/youtube2podbean-podbean:$TRAVIS_TAG
+docker push $DOCKER_USERNAME/youtube2podbean-podbean:latest
 
-docker build --rm -f "services/wordpress.Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean-wordpress:$TRAVIS_TAG" --build-arg BASE_VERSION=$TRAVIS_TAG ..
-docker tag "$DOCKER_USRENAME/youtube2podbean-wordpress:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean-wordpress:latest"
-docker push $DOCKER_USRENAME/youtube2podbean-wordpress:$TRAVIS_TAG
-docker push $DOCKER_USRENAME/youtube2podbean-wordpress:latest
+docker build --rm -f "services/wordpress.Dockerfile" -t "$DOCKER_USERNAME/youtube2podbean-wordpress:$TRAVIS_TAG" --build-arg BASE_VERSION=$TRAVIS_TAG ..
+docker tag "$DOCKER_USERNAME/youtube2podbean-wordpress:$TRAVIS_TAG" "$DOCKER_USERNAME/youtube2podbean-wordpress:latest"
+docker push $DOCKER_USERNAME/youtube2podbean-wordpress:$TRAVIS_TAG
+docker push $DOCKER_USERNAME/youtube2podbean-wordpress:latest
 
-docker build --rm -f "services/youtube.Dockerfile" -t "$DOCKER_USRENAME/youtube2podbean-youtube:$TRAVIS_TAG" --build-arg BASE_VERSION=$TRAVIS_TAG ..
-docker tag "$DOCKER_USRENAME/youtube2podbean-youtube:$TRAVIS_TAG" "$DOCKER_USRENAME/youtube2podbean-youtube:latest"
-docker push $DOCKER_USRENAME/youtube2podbean-youtube:$TRAVIS_TAG
-docker push $DOCKER_USRENAME/youtube2podbean-youtube:latest
+docker build --rm -f "services/youtube.Dockerfile" -t "$DOCKER_USERNAME/youtube2podbean-youtube:$TRAVIS_TAG" --build-arg BASE_VERSION=$TRAVIS_TAG ..
+docker tag "$DOCKER_USERNAME/youtube2podbean-youtube:$TRAVIS_TAG" "$DOCKER_USERNAME/youtube2podbean-youtube:latest"
+docker push $DOCKER_USERNAME/youtube2podbean-youtube:$TRAVIS_TAG
+docker push $DOCKER_USERNAME/youtube2podbean-youtube:latest
 
 docker logout
